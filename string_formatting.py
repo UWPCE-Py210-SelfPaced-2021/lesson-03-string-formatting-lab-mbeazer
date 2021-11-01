434d9ac8094f21fc03752a891836c662d0dc6895
mbeazer <mikebeazer@hotmail.com>
2021-11-01 15:07:05 -0700

    Task 2 complete using f-string

--- string_formatting.py
+++ string_formatting.py
@@ -1,2 +1,6 @@
 # Task 1
 print('file_00{:d} :   {:.2f}, {:.2e}, {:.2e}'.format(2, 123.4567, 10000, 12345.67))
+
+# Task 2
+elements = (2, 123.4567, 10000, 12345.67)
+print(f'file_00{elements[0]} :   {elements[1]:.2f}, {elements[2]:.2e}, {elements[3]:.2e}')
