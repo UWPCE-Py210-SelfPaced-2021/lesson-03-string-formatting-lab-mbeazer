792cbf9f92d59ad3edc7a14bf5bcc03991dbb10a
mbeazer <mikebeazer@hotmail.com>
2021-11-01 17:45:33 -0700

    Task 3 complete, uses f-string and loops through length of tuple

--- string_formatting.py
+++ string_formatting.py
@@ -4,3 +4,22 @@ print('file_00{:d} :   {:.2f}, {:.2e}, {:.2e}'.format(2, 123.4567, 10000, 12345.
 # Task 2
 elements = (2, 123.4567, 10000, 12345.67)
 print(f'file_00{elements[0]} :   {elements[1]:.2f}, {elements[2]:.2e}, {elements[3]:.2e}')
+
+
+# Task 3
+def formatter(in_tuple):
+    form_string = f'The {len(in_tuple)} numbers are: '
+    for i in range(len(in_tuple)):
+        """Returns last number with no comma"""
+        if i == len(in_tuple) - 1:
+            form_string = form_string + '{:d}'
+        else:
+            form_string = form_string + '{:d}, '
+
+    return form_string.format(*in_tuple)
+
+
+print(formatter((2, 3, 5, 7, 9)))
+
+# Task 4
+
